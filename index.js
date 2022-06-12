cbf0ebb24bf2a3f6188159aef511e3545159b239
3165254905 <runfeng.yu@foxmail.com>
2022-06-12 13:30:46 +0800

    22061213300

--- index.js
+++ index.js
@@ -2,23 +2,64 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+const Schema = mongoose.Schema;
+const urlSchema = new Schema({
+  url: String,
+  shortUrl: Number
+});
+const Url = mongoose.model('Url', urlSchema);
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', (req, res) => {
+  const url = req.body.url;
+  if (!/^(http|https):\/\/[^\s]+.com$/.test(url)) {
+    res.json({ error: 'invalid url' });
+    return;
+  }
+  const shortUrl = 1;
+  const urlModel = new Url({
+    url: url,
+    shortUrl: shortUrl
+  });
+  urlModel.save((err, data) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(data);
+    }
+  });
+  res.json({
+    original_url: url,
+    short_url: shortUrl
+  });
 });
 
-app.listen(port, function() {
+app.get('/api/shorturl/:shortUrl', (req, res) => {
+  const shortUrl = req.params.shortUrl;
+  Url.findOne({ shortUrl: shortUrl }, (err, data) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.redirect(data.url);
+    }
+  });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
